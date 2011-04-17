889c46c8586816b16fd68f7ec6c98874b318499d
luis petek <lmpetek@gmail.com>
2011-04-17 16:51:55 -0300

    validaciones en cliente razon social, cuit

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,4 +2,8 @@ class Cliente < ActiveRecord::Base
   has_many :factura
   has_many :recibo
   has_many :notacredito
+  # validates_presence_of :razonsocial, :codigo, :cuit 
+  validates :cuit, :presence => true, :length => { :maximum => 11 }
+  validates :razonsocial, :presence => true
+  validates :codigo, :presence => true
 end
