e65eb0003ee9efb5389010d9bf967834a7a67467
luis petek <lmpetek@gmail.com>
2011-05-02 20:35:58 -0300

    seguridad application

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,21 +5,23 @@ $(document).ready(function()
 {
   // jQuery datepicker for formtastic (see http://gist.github.com/271377)
   $('input.ui-datepicker').datepicker({ dateFormat: 'yy-mm-dd'});
+
+	$('.accordion').accordion({
+	  collapsible: true,
+	  active: false,
+	  autoHeight: true
+	});
+
+	$("#filters .clean-filters").bind('click', function(event){
+	   event.preventDefault();
+	   $("fieldset.inputs :input").val("");
+	 });
+
+	 $("#filters fieldset.inputs :input").each(function(){
+	   if( $(this).val() != '' ){
+	     $('.accordion').accordion("activate");
+	   }
+	 });
+
 });
 
-$('.accordion').accordion({
-  collapsible: true,
-  active: false,
-  autoHeight: true
-});
-
-$("#filters .clean-filters").bind('click', function(event){
-   event.preventDefault();
-   $("fieldset.inputs :input").val("");
- });
- 
- $("#filters fieldset.inputs :input").each(function(){
-   if( $(this).val() != '' ){
-     $('.accordion').accordion("activate");
-   }
- });
